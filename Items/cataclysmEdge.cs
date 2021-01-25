1f3ced8fa9e8f16b672c213d47368dec5e852b20
JoaquimRefs <joao.pd.sousa@gmail.com>
2021-01-25 15:14:45 -0100

    BS-CE- added more functionallity and Utils

--- Items/cataclysmEdge.cs
+++ Items/cataclysmEdge.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using warcraftweaponscollection.Utils;
 
 namespace warcraftweaponscollection.Items
 {
     public class cataclysmEdge : ModItem
     {
+
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Cataclysm's Edge"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
@@ -15,21 +19,24 @@ namespace warcraftweaponscollection.Items
 
 		public override void SetDefaults()
 		{
+			item.Size = new Vector2(40);
+
 			item.damage = 50;
 			item.crit = 10;
+			item.knockBack = 6;
 			item.melee = true;
 			item.magic = true;
-			item.width = 40;
-			item.height = 40;
-			item.useTime = 20;
-			item.useAnimation = 20;
-			item.useStyle = 1;
+
+			item.useTime = 40;
+			item.useAnimation = 40;
+			item.useStyle = ItemUseStyleID.SwingThrow;
 			item.useTurn = true;
-			item.knockBack = 6;
-			item.value = 10000;
-			item.rare = 11;
-			item.UseSound = SoundID.DD2_DarkMageAttack;
 			item.autoReuse = true;
+
+			item.value = Item.sellPrice(gold: 150);
+			item.rare = ItemRarityID.Purple;
+
+			item.UseSound = SoundID.DD2_DarkMageAttack;
 		}
 
 		public override void AddRecipes()
@@ -41,77 +48,33 @@ namespace warcraftweaponscollection.Items
 			recipe.AddRecipe();
 		}
 
-		public override bool AltFunctionUse(Player player)
-		{
-			return true;
-		}
-
-		public override bool CanUseItem(Player player)
-		{
-			if (player.altFunctionUse == 2)
-			{
-				item.useStyle = ItemUseStyleID.Stabbing;
-				item.useTime = 20;
-				item.useAnimation = 20;
-				item.damage = 50;
-				item.shoot = ProjectileID.Bee;
-			}
-			else
-			{
-				item.useStyle = ItemUseStyleID.SwingThrow;
-				item.useTime = 40;
-				item.useAnimation = 40;
-				item.damage = 100;
-				item.shoot = ProjectileID.None;
-			}
-			return base.CanUseItem(player);
-		}
-
 		public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
-		{
-			if (player.altFunctionUse == 2)
-			{
-				target.AddBuff(BuffID.Ichor, 60);
-			}
-			else
-			{
+		{		
 				target.AddBuff(BuffID.OnFire, 60);
-			}
 		}
 
-		public override void MeleeEffects(Player player, Rectangle hitbox)
-		{
-			if (Main.rand.NextBool(3))
+        public override void ModifyHitNPC(Player player, NPC target, ref int damage, ref float knockBack, ref bool crit)
+        {
+
+			if (IsUndead.isUndead(target))
 			{
-				if (player.altFunctionUse == 2)
-				{
-					int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 169, 0f, 0f, 100, default(Color), 2f);
-					Main.dust[dust].noGravity = true;
-					Main.dust[dust].velocity.X += player.direction * 2f;
-					Main.dust[dust].velocity.Y += 0.2f;
-				}
-				else
-				{
-					int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.Fire, player.velocity.X * 0.2f + (float)(player.direction * 3), player.velocity.Y * 0.2f, 100, default(Color), 2.5f);
-					Main.dust[dust].noGravity = true;
-				}
+				damage *= 2;
 			}
-		}
+        }
 
-		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        public override void HoldItem(Player player)
 		{
-			// Fix the speedX and Y to point them horizontally.
-			speedX = new Vector2(speedX, speedY).Length() * (speedX > 0 ? 1 : -1);
-			speedY = 0;
-			// Add random Rotation
-			Vector2 speed = new Vector2(speedX, speedY);
-			speed = speed.RotatedByRandom(MathHelper.ToRadians(30));
-			// Change the damage since it is based off the weapons damage and is too high
-			damage = (int)(damage * .1f);
-			speedX = speed.X;
-			speedY = speed.Y;
-			return true;
+			if (Main.rand.Next(player.itemAnimation > 0 ? 40 : 80) == 0)
+			{
+				Dust.NewDust(new Vector2(player.itemLocation.X + 16f * player.direction, player.itemLocation.Y - 14f * player.gravDir), 4, 4, DustID.Fire);
+			}
+			Vector2 position = player.RotatedRelativePoint(new Vector2(player.itemLocation.X + 12f * player.direction + player.velocity.X, player.itemLocation.Y - 14f + player.velocity.Y), true);
+			Lighting.AddLight(position, 1f, 1f, 1f);
+
+			
+
 		}
 
+		
 	}
 }
---
+++ Utils/IsUndead.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+
+namespace warcraftweaponscollection.Utils
+{
+    public class IsUndead
+    {
+		public static bool isUndead(NPC target)
+		{
+			switch (target.type)
+			{
+				case NPCID.Zombie:
+					return true;
+				case NPCID.ZombieDoctor:
+					return true;
+				case NPCID.ZombieElf:
+					return true;
+				case NPCID.ZombieElfBeard:
+					return true;
+				case NPCID.ZombieElfGirl:
+					return true;
+				case NPCID.ZombieEskimo:
+					return true;
+				case NPCID.ZombieMushroom:
+					return true;
+				case NPCID.ZombieMushroomHat:
+					return true;
+				case NPCID.ZombiePixie:
+					return true;
+				case NPCID.ZombieRaincoat:
+					return true;
+				case NPCID.ZombieSuperman:
+					return true;
+				case NPCID.ZombieSweater:
+					return true;
+				case NPCID.ZombieXmas:
+					return true;
+				case NPCID.ArmedZombie:
+					return true;
+				case NPCID.ArmedZombieCenx:
+					return true;
+				case NPCID.ArmedZombieEskimo:
+					return true;
+				case NPCID.ArmedZombiePincussion:
+					return true;
+				case NPCID.ArmedZombieSlimed:
+					return true;
+				case NPCID.ArmedZombieSwamp:
+					return true;
+				case NPCID.ArmedZombieTwiggy:
+					return true;
+				case NPCID.BaldZombie:
+					return true;
+				case NPCID.BigBaldZombie:
+					return true;
+				case NPCID.BigFemaleZombie:
+					return true;
+				case NPCID.BigPincushionZombie:
+					return true;
+				case NPCID.BigRainZombie:
+					return true;
+				case NPCID.BigSlimedZombie:
+					return true;
+				case NPCID.BigSwampZombie:
+					return true;
+				case NPCID.BigTwiggyZombie:
+					return true;
+				case NPCID.BigZombie:
+					return true;
+				case NPCID.BloodZombie:
+					return true;
+				case NPCID.FemaleZombie:
+					return true;
+				case NPCID.PincushionZombie:
+					return true;
+				case NPCID.SlimedZombie:
+					return true;
+				case NPCID.SmallBaldZombie:
+					return true;
+				case NPCID.SmallFemaleZombie:
+					return true;
+				case NPCID.SmallPincushionZombie:
+					return true;
+				case NPCID.SmallRainZombie:
+					return true;
+				case NPCID.SmallSlimedZombie:
+					return true;
+				case NPCID.SmallSwampZombie:
+					return true;
+				case NPCID.SmallTwiggyZombie:
+					return true;
+				case NPCID.SmallZombie:
+					return true;
+				case NPCID.SwampZombie:
+					return true;
+				case NPCID.TwiggyZombie:
+					return true;
+				case NPCID.UndeadMiner:
+					return true;
+					
+				case NPCID.UndeadViking:
+					return true;
+					
+				case NPCID.Skeleton:
+					return true;
+					
+				case NPCID.SkeletonAlien:
+					return true;
+					
+				case NPCID.SkeletonArcher:
+					return true;
+					
+				case NPCID.SkeletonAstonaut:
+					return true;
+					
+				case NPCID.SkeletonCommando:
+					return true;
+					
+				case NPCID.SkeletonMerchant:
+					return true;
+					
+				case NPCID.SkeletonSniper:
+					return true;
+					
+				case NPCID.SkeletonTopHat:
+					return true;
+					
+				case NPCID.ArmoredSkeleton:
+					return true;
+					
+				case NPCID.BigHeadacheSkeleton:
+					return true;
+					
+				case NPCID.BigMisassembledSkeleton:
+					return true;
+					
+				case NPCID.BigPantlessSkeleton:
+					return true;
+					
+				case NPCID.BigSkeleton:
+					return true;
+					
+				case NPCID.BoneThrowingSkeleton:
+					return true;
+					
+				case NPCID.BoneThrowingSkeleton3:
+					return true;
+					
+				case NPCID.BoneThrowingSkeleton2:
+					return true;
+					
+				case NPCID.BoneThrowingSkeleton4:
+					return true;
+					
+				case NPCID.DD2SkeletonT1:
+					return true;
+					
+				case NPCID.DD2SkeletonT3:
+					return true;
+					
+				case NPCID.GreekSkeleton:
+					return true;
+					
+				case NPCID.HeadacheSkeleton:
+					return true;
+					
+				case NPCID.HeavySkeleton:
+					return true;
+					
+				case NPCID.MisassembledSkeleton:
+					return true;
+					
+				case NPCID.PantlessSkeleton:
+					return true;
+					
+				case NPCID.SmallHeadacheSkeleton:
+					return true;
+					
+				case NPCID.SmallMisassembledSkeleton:
+					return true;
+					
+				case NPCID.SmallPantlessSkeleton:
+					return true;
+					
+				case NPCID.SmallSkeleton:
+					return true;
+					
+				case NPCID.TacticalSkeleton:
+					return true;
+					
+				case NPCID.SkeletronHand:
+					return true;
+					
+				case NPCID.SkeletronHead:
+					return true;
+					
+				case NPCID.SkeletronPrime:
+					return true;
+					
+				case NPCID.BoneLee:
+					return true;
+					
+				case NPCID.BoneSerpentBody:
+					return true;
+					
+				case NPCID.BoneSerpentHead:
+					return true;
+					
+				case NPCID.BoneSerpentTail:
+					return true;
+					
+				case NPCID.AngryBones:
+					return true;
+					
+				case NPCID.AngryBonesBig:
+					return true;
+					
+				case NPCID.AngryBonesBigHelmet:
+					return true;
+					
+				case NPCID.AngryBonesBigMuscle:
+					return true;
+					
+				case NPCID.BigBoned:
+					return true;
+
+				case NPCID.BlueArmoredBones:
+					return true;
+					
+				case NPCID.BlueArmoredBonesMace:
+					return true;
+					
+				case NPCID.BlueArmoredBonesSword:
+					return true;
+					
+				case NPCID.DoctorBones:
+					return true;
+					
+				case NPCID.HellArmoredBones:
+					return true;
+					
+				case NPCID.HellArmoredBonesMace:
+					return true;
+					
+				case NPCID.HellArmoredBonesSpikeShield:
+					return true;
+					
+				case NPCID.HellArmoredBonesSword:
+					return true;
+					
+				case NPCID.RustyArmoredBonesSwordNoArmor:
+					return true;
+					
+				case NPCID.RustyArmoredBonesFlail:
+					return true;
+					
+				case NPCID.ShortBones:
+					return true;
+				default:
+					return false;
+			}
+		}
+	}
+}
