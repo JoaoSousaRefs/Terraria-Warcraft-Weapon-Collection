1046b97d6add1779b38321fe63fc8bdadfddb31b
JoaquimRefs <joao.pd.sousa@gmail.com>
2021-01-25 10:59:29 -0100

    BS-CE - Placeholder png, dual use item template

---
+++ Items/cataclysmEdge.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace warcraftweaponscollection.Items
+{
+    public class cataclysmEdge : ModItem
+    {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Cataclysm's Edge"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("This is a basic modded sword.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 50;
+			item.crit = 10;
+			item.melee = true;
+			item.magic = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.useTurn = true;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 11;
+			item.UseSound = SoundID.DD2_DarkMageAttack;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 1);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override bool AltFunctionUse(Player player)
+		{
+			return true;
+		}
+
+		public override bool CanUseItem(Player player)
+		{
+			if (player.altFunctionUse == 2)
+			{
+				item.useStyle = ItemUseStyleID.Stabbing;
+				item.useTime = 20;
+				item.useAnimation = 20;
+				item.damage = 50;
+				item.shoot = ProjectileID.Bee;
+			}
+			else
+			{
+				item.useStyle = ItemUseStyleID.SwingThrow;
+				item.useTime = 40;
+				item.useAnimation = 40;
+				item.damage = 100;
+				item.shoot = ProjectileID.None;
+			}
+			return base.CanUseItem(player);
+		}
+
+		public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+		{
+			if (player.altFunctionUse == 2)
+			{
+				target.AddBuff(BuffID.Ichor, 60);
+			}
+			else
+			{
+				target.AddBuff(BuffID.OnFire, 60);
+			}
+		}
+
+		public override void MeleeEffects(Player player, Rectangle hitbox)
+		{
+			if (Main.rand.NextBool(3))
+			{
+				if (player.altFunctionUse == 2)
+				{
+					int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 169, 0f, 0f, 100, default(Color), 2f);
+					Main.dust[dust].noGravity = true;
+					Main.dust[dust].velocity.X += player.direction * 2f;
+					Main.dust[dust].velocity.Y += 0.2f;
+				}
+				else
+				{
+					int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.Fire, player.velocity.X * 0.2f + (float)(player.direction * 3), player.velocity.Y * 0.2f, 100, default(Color), 2.5f);
+					Main.dust[dust].noGravity = true;
+				}
+			}
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			// Fix the speedX and Y to point them horizontally.
+			speedX = new Vector2(speedX, speedY).Length() * (speedX > 0 ? 1 : -1);
+			speedY = 0;
+			// Add random Rotation
+			Vector2 speed = new Vector2(speedX, speedY);
+			speed = speed.RotatedByRandom(MathHelper.ToRadians(30));
+			// Change the damage since it is based off the weapons damage and is too high
+			damage = (int)(damage * .1f);
+			speedX = speed.X;
+			speedY = speed.Y;
+			return true;
+		}
+
+	}
+}
