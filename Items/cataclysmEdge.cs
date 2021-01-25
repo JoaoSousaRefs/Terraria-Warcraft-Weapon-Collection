022d3c38ad12323684a21e6bad82971040945859
JoaquimRefs <joao.pd.sousa@gmail.com>
2021-01-25 20:25:20 -0100

    BS-CE - Upscaled the sprite (40 to 80) - added Dust effect - added weapon functionality (chance on hit and ative skill)

--- Items/cataclysmEdge.cs
+++ Items/cataclysmEdge.cs
@@ -14,21 +14,24 @@ namespace warcraftweaponscollection.Items
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Cataclysm's Edge"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-			Tooltip.SetDefault("This is a basic modded sword.");
+			Tooltip.SetDefault("Chance on hit: Set the target Burning. \n" +
+				"Attacking Burning enemies results in a critical hit.");
+			
+			
 		}
 
 		public override void SetDefaults()
 		{
-			item.Size = new Vector2(40);
+			item.Size = new Vector2(80);
 
-			item.damage = 50;
-			item.crit = 10;
-			item.knockBack = 6;
+			item.damage = 80;
+			item.knockBack = 10;
 			item.melee = true;
 			item.magic = true;
+			item.crit = 5;
 
-			item.useTime = 40;
-			item.useAnimation = 40;
+			item.useTime = 50;
+			item.useAnimation = 50;
 			item.useStyle = ItemUseStyleID.SwingThrow;
 			item.useTurn = true;
 			item.autoReuse = true;
@@ -36,7 +39,7 @@ namespace warcraftweaponscollection.Items
 			item.value = Item.sellPrice(gold: 150);
 			item.rare = ItemRarityID.Purple;
 
-			item.UseSound = SoundID.DD2_DarkMageAttack;
+			item.UseSound = SoundID.DD2_OgreGroundPound;
 		}
 
 		public override void AddRecipes()
@@ -49,32 +52,36 @@ namespace warcraftweaponscollection.Items
 		}
 
 		public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
-		{		
-				target.AddBuff(BuffID.OnFire, 60);
+		{
+			Random mathRandom = new Random();
+			var debuffChange = mathRandom.Next(1, 100);
+
+			//theres a 25% chance per hit to apply Onfire debuff
+            if (debuffChange < 50 && debuffChange > 25)
+            {
+				target.AddBuff(BuffID.OnFire, 160);
+            }
+            
 		}
 
         public override void ModifyHitNPC(Player player, NPC target, ref int damage, ref float knockBack, ref bool crit)
         {
-
-			if (IsUndead.isUndead(target))
+			//If the target has Onfire debuff, the next hit should be a 100% crit chance
+			if (target.HasBuff(BuffID.OnFire))
 			{
-				damage *= 2;
+				crit = true;
 			}
         }
 
-        public override void HoldItem(Player player)
+		public override void MeleeEffects(Player player, Rectangle hitbox)
 		{
-			if (Main.rand.Next(player.itemAnimation > 0 ? 40 : 80) == 0)
+			if (Main.rand.NextBool(3))
 			{
-				Dust.NewDust(new Vector2(player.itemLocation.X + 16f * player.direction, player.itemLocation.Y - 14f * player.gravDir), 4, 4, DustID.Fire);
+					int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 127, 1f, 1f, 0, default(Color), 2.105263f);
+					Main.dust[dust].noGravity = true;
+					Main.dust[dust].velocity.X += player.direction * 2f;
+					Main.dust[dust].velocity.Y += 0.2f;
 			}
-			Vector2 position = player.RotatedRelativePoint(new Vector2(player.itemLocation.X + 12f * player.direction + player.velocity.X, player.itemLocation.Y - 14f + player.velocity.Y), true);
-			Lighting.AddLight(position, 1f, 1f, 1f);
-
-			
-
 		}
-
-		
 	}
 }
